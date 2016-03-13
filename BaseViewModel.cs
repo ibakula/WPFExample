3cb7394a357aecb0d567ed22a306e7a660f84c53
aero1005 <ivan.bakula@wisp.hr>
2016-03-13 14:50:29 +0100

    initial commit

---
+++ BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Events
+{
+    public class PropertyObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+ 
+        private void OnPropertyChangeEvent(string link)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(link));
+        }
+    }
+
+    public class BaseViewModel : PropertyObject
+    {
+        public string test;
+    }
+}
