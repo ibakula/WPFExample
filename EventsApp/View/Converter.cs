9071a649b1eca0ff979f3b4f57b1e699b6f3a150
Ivan Bakula <ivan.bakula@outlook.com>
2016-03-17 17:08:35 +0100

    Connection required to get news, no caching

--- EventsApp/View/Converter.cs
+++ EventsApp/View/Converter.cs
@@ -9,6 +9,7 @@ using EventsApp.ViewModel;
 using System.ServiceModel.Syndication;
 using System.IO;
 using System.Xml;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace EventsApp.View
 {
@@ -16,22 +17,33 @@ namespace EventsApp.View
     {
         static public void Convert(string hostUrl, string suffixUrl, BaseViewModel bvm) // https://channel9.msdn.com/Events/Build/2015/RSS / https://s.ch9.ms/Events/Build/2015/RSS
         {
-            string xmlData = String.Empty;
+            byte[] data = null;
             using (WebClient client = new WebClient())
             {
                 client.BaseAddress = hostUrl;
-                byte[] data = client.DownloadData(suffixUrl);
-                xmlData = Encoding.ASCII.GetString(data);
+                try
+                {
+                    data = client.DownloadData(suffixUrl);
+                }
+#pragma warning disable 168
+                catch (Exception e) // Exception could occur if document unreachable.
+                {
+                    // ToDo: Handle
+                }
+#pragma warning restore 168
             }
-            using (StreamWriter sw = new StreamWriter(File.Open("rss.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read)))
+            if (data == null) // Failed to get?
+                return;
+            using (MemoryStream ms = new MemoryStream())
             {
-                sw.Write(xmlData);
-            }
-            using (XmlReader xr = XmlReader.Create("rss.xml"))
-            {
-                xr.Read();
-                SyndicationFeed syn = SyndicationFeed.Load(xr);
-                bvm.GetFeed = syn;
+                ms.Write(data, 0, data.Length);
+                ms.Position = 0;
+                using (XmlReader xr = XmlReader.Create(ms))
+                {
+                    xr.Read();
+                    SyndicationFeed syn = SyndicationFeed.Load(xr);
+                    bvm.GetFeed = syn;
+                }
             }
         }
     }
